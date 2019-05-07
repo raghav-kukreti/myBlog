2c42c22956815c99ac142999a133a876bbbb79b4
Raghav Kukreti <raghav.kukreti@gmail.com>
2019-05-07 12:55:47 +0530

    Last night changes; basic server running

--- lib/routes/crm_routes.ts
+++ lib/routes/crm_routes.ts
@@ -2,6 +2,18 @@ import {Request, Response} from "express";
 
 export class Routes {       
     public routes(app): void {          
-        app.route('/').get((req, res) => res.json({name : "hello, check check 123"}));
+        app.route('/').get((req: Request, res: Response) => res.json({name : "hello, check check 123"}));
+        
+        app.route('/contact').get((req: Request, res: Response) => res.json({contact : "get_complete.wav"}));
+        app.route('/contact').post((req: Request, res: Response) => res.json({contact : "post_complete.wav"}));
+
+        app.route('/contact/:contactID')
+        .get(((req: Request, res: Response) => {
+            res.json({contact : "get_complete.wav"})
+        })).put(((req: Request, res: Response) => {
+            res.json({contact : "put_complete.wav"})
+        })).delete(((req: Request, res: Response) => {
+            res.json({contact : "delete_complete.wav"})
+        }));
     }
 }
